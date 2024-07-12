c73c70203060533fa5b409b947934d43cadc6c90
MariusSilviuHriscu <hriscumarius65536@gmail.com>
2024-07-12 21:17:46 +0300

    info string

--- ck2_savefile/info_representation.py
+++ ck2_savefile/info_representation.py
@@ -1,6 +1,11 @@
 from pathlib import Path
 import typing
-import json
+from dataclasses import dataclass
+
+@dataclass
+class SimpleInfoChange :
+    line_number : int
+    new_line : str
 
 class InfoRepresentation(typing.Protocol):
     
@@ -8,30 +13,34 @@ class InfoRepresentation(typing.Protocol):
     def create(raw_into : str) -> typing.Self:
         pass
     
-    def to_raw_string(self) -> str:
+    def to_raw_string(self) -> typing.Generator[str] | str:
         pass
     @staticmethod
     def corresponds(raw_info:str) ->bool:
         pass
-    def change_value(self , other : typing.Any) -> None:
+    def change_value(self , other : typing.Any) -> list[tuple[int , str]]:
         pass
 
+    
 class OneLineKeyValueInfo:
-    def __init__(self , 
+    def __init__(self ,
+                 index : int ,
                  data_key : str ,
                  data_value : str , 
                  start_spaces : int ,
                  end_spaces : int):
+        self.index = index
         self.data_key = data_key
         self.data_value = data_value
         self.start_spaces= start_spaces
         self.end_spaces = end_spaces
     @staticmethod
-    def create(raw_info : str) -> typing.Self:
+    def create(raw_info : str , index : int) -> typing.Self:
         start_spaces = raw_info.count('\t')
         end_spaces = raw_info.count('\n')
         data_name , data_value = raw_info.replace('\t' , '').replace('\n', '').split('=')
         return OneLineKeyValueInfo(
+            index = index ,
             data_key = data_name,
             data_value = data_value,
             start_spaces = start_spaces, 
@@ -50,30 +59,38 @@ class OneLineKeyValueInfo:
     def to_raw_string(self) -> str:
         return "\t"*self.start_spaces + f'{self.data_key}={self.data_value}' + "\n"*self.end_spaces
     
-    def change_value(self , value : str) :
+    def change_value(self , value : str) -> SimpleInfoChange:
         self.data_value = value
+        
+        return SimpleInfoChange(
+            line_number = self.index,
+            new_line = self.to_raw_string()
+        )
 class MultiKeyValueInfo():
     def __init__(self,
+                 index : int,
                  values : list[OneLineKeyValueInfo],
                  start_space : int ,
                  end_spaces : int ) :
+        self.index = index
         self.values = values
         self.start_space = start_space
         self.end_spaces = end_spaces
     
     @staticmethod
-    def create(raw_info : str) -> typing.Self :
+    def create(raw_info : str , index : int) -> typing.Self :
         
         start_spaces = raw_info.count('\t')
         end_spaces = raw_info.count('\n')
         
         data_raw = raw_info.replace('\n' , '').replace('\t ', '')
         pairs = data_raw.split(' ')
-        data = [OneLineKeyValueInfo.create(raw_info=x) 
+        data = [OneLineKeyValueInfo.create(raw_info=x , index = index) 
                 for x in pairs if '=' in x]
 
         
         return MultiKeyValueInfo(
+            index = index,
             values=data,
             start_space=start_spaces,
             end_spaces=end_spaces
@@ -92,22 +109,39 @@ class MultiKeyValueInfo():
                 ' '.join((x.to_raw_string() for x in self.values)) +
                 '\n' * self.end_spaces
         )
+    def change(self, key : str , new_value : str)-> SimpleInfoChange:
+        for value in self.values:
+            if value.data_key == key:
+                value.data_value = new_value
+        
+                return SimpleInfoChange(
+                    line_number = self.index,
+                    new_line = self.to_raw_string()
+                )
+        raise Exception("Couldn't find the key in the info .")
 
 class OneLineListInfo:
-    def __init__(self , info_list : list[str],start_spaces : int , end_spaces : int):
+    def __init__(self ,
+                 index : int, 
+                 info_list : list[str],
+                 start_spaces : int ,
+                 end_spaces : int
+                 ):
+        self.index = index
         self.info_list = info_list
         self.start_spaces = start_spaces
         self.end_spaces = end_spaces
     
     @staticmethod
-    def create(raw_info : str) -> typing.Self:
+    def create(raw_info : str , index : int) -> typing.Self:
         
         start_spaces = raw_info.count('\t')
         end_spaces = raw_info.count('\n')
         
         data = raw_info.replace('\t' , '').replace('\n' , '').split(' ')
         
-        return OneLineListInfo(info_list = data,
+        return OneLineListInfo(index= index ,
+                               info_list = data,
                                start_spaces = start_spaces,
                                end_spaces = end_spaces
                                )
@@ -119,28 +153,40 @@ class OneLineListInfo:
     def to_raw_string(self) -> str:
         return '\t' * self.start_spaces + ' '.join(self.info_list) + '\n' * self.end_spaces
     
-    def change_value(self , other_list : list[str]):
+    def change_value(self , other_list : list[str])-> SimpleInfoChange:
         self.info_list = other_list
+        
+        return SimpleInfoChange(
+            line_number = self.index,
+            new_line = self.to_raw_string()
+        )
 
 class OneLineKeyListInfo:
     
-    def __init__(self , data_key : str , data_list : OneLineListInfo,start_spaces : int , end_spaces : int):
-        
+    def __init__(self ,
+                 index : int ,
+                 data_key : str ,
+                 data_list : OneLineListInfo,
+                 start_spaces : int ,
+                 end_spaces : int):
+        self.index = index
         self.data_key = data_key
         self.data_list = data_list
         self.start_spaces = start_spaces
         self.end_spaces = end_spaces
     
-    def create(raw_info : str) -> typing.Self:
+    def create(raw_info : str , index : int) -> typing.Self:
         
         start_spaces = raw_info.count('\t')
         end_spaces = raw_info.count('\n')
         
         data_key , data_list_str = raw_info.replace('\t' , '').replace('\n' , '').split('=')
         data_list = OneLineListInfo.create(
-                    raw_info = data_list_str.replace('{','\t').replace('}','\n')
+                    raw_info = data_list_str.replace('{','\t').replace('}','\n'),
+                    index = index
         )
         return OneLineKeyListInfo(
+            index = index ,
             data_key = data_key ,
             data_list = data_list,
             start_spaces=start_spaces,
@@ -154,15 +200,25 @@ class OneLineKeyListInfo:
         data_list_str = self.data_list.to_raw_string().replace('\t','{').replace('\n','}')
         
         return "\t" *self.start_spaces + f'{self.data_key}={data_list_str}' + "\n"*self.end_spaces
+    def change_value(self , other_list : list[str])-> SimpleInfoChange:
+        self.data_list = other_list
+        
+        return SimpleInfoChange(
+            line_number = self.index,
+            new_line = self.to_raw_string()
+        )
 class DictInfo:
     
-    def __init__(self , 
+    def __init__(self ,
+                 start_index : int,
+                 end_index : int ,
                  key : str ,
                  value : list[typing.Self | OneLineKeyListInfo | OneLineListInfo | OneLineKeyValueInfo],
                  start_spaces : int,
                  end_spaces : int
                  ):
-        
+        self.start_index = start_index
+        self.end_index = end_index
         self.key = key
         self.value = value
         self.start_spaces = start_spaces
@@ -174,7 +230,7 @@ class DictInfo:
         return raw_info[-2::] == '=\n'
 
     @staticmethod
-    def create( raw_key_data :str , ck2generator : typing.Generator[str,None,None] ) -> typing.Self:
+    def create( raw_key_data :str,start_index : int , ck2generator : typing.Generator[str,None,None] ) -> typing.Self:
         
         start_spaces = raw_key_data.count('\t')
         end_spaces = raw_key_data.count('\n')
@@ -183,42 +239,168 @@ class DictInfo:
         
         dict_data = []
         
-        open_parenthesis = next(ck2generator)
+        index_paranthesis , open_parenthesis = next(ck2generator)
+        
         if not open_parenthesis.endswith('{\n'):
             raise ValueError(f'Expected a curly bracket but only got {open_parenthesis}')
         
-        for line in ck2generator:
+        for index,line in ck2generator:
             
             if OneLineKeyValueInfo.corresponds(raw_info = line):
                 
-                new_value = OneLineKeyValueInfo.create(raw_info = line)
+                new_value = OneLineKeyValueInfo.create(raw_info = line , index = index)
                 dict_data.append(new_value)
             
             if MultiKeyValueInfo.corresponds(raw_info= line):
                 
-                new_value = MultiKeyValueInfo.create(raw_info= line)
+                new_value = MultiKeyValueInfo.create(raw_info= line , index = index)
                 dict_data.append(new_value)
             
             if OneLineListInfo.corresponds(raw_info = line):
                 
-                new_value = OneLineListInfo.create(raw_info = line)
+                new_value = OneLineListInfo.create(raw_info = line, index = index)
                 dict_data.append(new_value)
             
             if OneLineKeyListInfo.corresponds(raw_info = line):
                 
-                new_value = OneLineKeyListInfo.create(raw_info = line)
+                new_value = OneLineKeyListInfo.create(raw_info = line, index = index)
+                dict_data.append(new_value)
+            
+            if OptionalKeyDict.corresponds(raw_info=line):
+                new_value = OptionalKeyDict.create(first_line = line,start_index = index , ck2generator = ck2generator)
                 dict_data.append(new_value)
             
             if DictInfo.corresponds(raw_info = line):
                 
-                new_value = DictInfo.create(raw_key_data = line , ck2generator = ck2generator)
+                new_value = DictInfo.create(raw_key_data = line , start_index= index, ck2generator = ck2generator)
                 dict_data.append(new_value)
             
             if line.endswith('}\n') and '{' not in line:
                 return DictInfo(
+                    start_index = start_index ,
+                    end_index = index ,
                     key = key_data ,
                     value = dict_data,
                     start_spaces= start_spaces,
                     end_spaces = end_spaces
                 )
         raise Exception('something is wrong !')
+    
+    def to_raw_string(self) -> typing.Generator[str , None , None]:
+        
+        first_line : str = '\t' * self.start_spaces + f'{self.key}=' + '\n' * self.end_spaces
+        open_paranthesis : str = '\t' * self.start_spaces + '{' + '\n' * self.end_spaces
+        
+        yield first_line
+        yield open_paranthesis
+        
+        for value in self.value :
+            
+            raw_string = value.to_raw_string()
+            if isinstance(raw_string , str):
+                yield raw_string
+            else:
+                yield from raw_string
+        
+        end_parenthesis : str = '\t' * self.start_spaces + '}' + '\n' * self.end_spaces
+        
+        
+        yield end_parenthesis
+    
+    
+
+class OptionalKeyDict:
+    
+    def __init__(self ,
+                 start_index : int,
+                 end_index : int ,
+                 first_line_start : int ,
+                 last_line_start: int ,
+                 value : list[typing.Self | OneLineKeyListInfo | OneLineListInfo | OneLineKeyValueInfo | DictInfo],
+                 key : typing.Optional[str] = None ):
+        
+        self.start_index = start_index
+        self.end_index = end_index
+        self.first_line_start = first_line_start
+        self.last_line_start = last_line_start
+        self.value= value
+        self.key= key
+    
+    @staticmethod
+    def corresponds(raw_info : str):
+        
+        return raw_info.endswith('{\n') or raw_info.endswith('{\n')
+    
+    staticmethod
+    def create(first_line : str,start_index: int, ck2generator : typing.Generator[str,None,None] ) -> typing.Self:
+        
+        first_line_start = first_line.count('\t')
+        
+        key = None
+        
+        if '=' in first_line:
+            
+            key = first_line.split('=')[0].replace('\t','')
+        
+        dict_data = []
+        for index , line in ck2generator:
+            
+            if OneLineKeyValueInfo.corresponds(raw_info = line):
+                
+                new_value = OneLineKeyValueInfo.create(raw_info = line , index= index)
+                dict_data.append(new_value)
+            
+            if MultiKeyValueInfo.corresponds(raw_info= line):
+                
+                new_value = MultiKeyValueInfo.create(raw_info= line, index= index)
+                dict_data.append(new_value)
+            
+            if OneLineListInfo.corresponds(raw_info = line):
+                
+                new_value = OneLineListInfo.create(raw_info = line, index= index)
+                dict_data.append(new_value)
+            
+            if OneLineKeyListInfo.corresponds(raw_info = line):
+                
+                new_value = OneLineKeyListInfo.create(raw_info = line, index= index)
+                dict_data.append(new_value)
+            
+            if DictInfo.corresponds(raw_info = line):
+                
+                new_value = DictInfo.create(raw_key_data = line ,start_index=index, ck2generator = ck2generator)
+                dict_data.append(new_value)
+            
+            if OptionalKeyDict.corresponds(raw_info=line):
+                new_value = OptionalKeyDict.create(first_line = line,start_index = start_index , ck2generator = ck2generator)
+                dict_data.append(new_value)
+            
+            if line.endswith('}\n') and '{' not in line:
+                last_line_spaces = line.count('\t')
+                return OptionalKeyDict(
+                    start_index = start_index,
+                    end_index= index,
+                    first_line_start = first_line_start,
+                    last_line_start = last_line_spaces,
+                    value = dict_data,
+                    key = key
+                )
+        raise Exception('something is wrong !')
+    
+    def to_raw_string(self) -> typing.Generator[str , None , None]:
+        
+        first_line : str = '\t' * self.start_spaces + f'{self.key}='*(self.key is not None) + '{' + '\n' * self.end_spaces 
+        
+        yield first_line
+        
+        for value in self.value :
+            
+            raw_string = value.to_raw_string()
+            if isinstance(raw_string , str):
+                yield raw_string
+            else:
+                yield from raw_string
+        
+        end_parenthesis : str = '\t' * self.start_spaces + '}' + '\n' * self.end_spaces
+        
+        
+        yield end_parenthesis
--- ck2_savefile/parser.py
+++ ck2_savefile/parser.py
@@ -1,7 +1,5 @@
-from distutils.command.build_scripts import first_line_re
 from pathlib import Path
 import typing
-import json
 
 from ck2_savefile.info_representation import (
     InfoRepresentation,
@@ -9,7 +7,8 @@ from ck2_savefile.info_representation import (
     OneLineListInfo,
     OneLineKeyValueInfo,
     DictInfo,
-    MultiKeyValueInfo
+    MultiKeyValueInfo,
+    OptionalKeyDict
     )
 from ck2_savefile.response import ParseResponse
 
@@ -22,43 +21,49 @@ class SaveFileParser:
     @staticmethod
     def read_file_line_by_line(file_path : Path):
         with open(file_path, 'r') as file:
-            for line in file:
-                yield line
+            for index , line in enumerate(file):
+                yield index , line
     
     def one_line_data(self , line) -> bool:
         return '"' in line
     def _parse_data(self, 
                     generator : typing.Generator[str, None, None]
                     ) -> typing.Generator[InfoRepresentation , None ,None]:
-        for line in generator:
+        for index , line in generator:
         
             if OneLineKeyValueInfo.corresponds(raw_info = line):
                     
-                new_value = OneLineKeyValueInfo.create(raw_info = line)
+                new_value = OneLineKeyValueInfo.create(raw_info = line, index= index)
                 yield new_value
                 continue
             
             if MultiKeyValueInfo.corresponds(raw_info= line):
                 
-                new_value = MultiKeyValueInfo.create(raw_info= line)
+                new_value = MultiKeyValueInfo.create(raw_info= line, index= index)
                 yield new_value
                 continue
             
             if OneLineListInfo.corresponds(raw_info = line):
                 
-                new_value = OneLineListInfo.create(raw_info = line)
+                new_value = OneLineListInfo.create(raw_info = line, index= index)
                 yield new_value
                 continue
             
             if OneLineKeyListInfo.corresponds(raw_info = line):
                 
-                new_value = OneLineKeyListInfo.create(raw_info = line)
+                new_value = OneLineKeyListInfo.create(raw_info = line, index= index)
                 yield new_value
                 continue
             
             if DictInfo.corresponds(raw_info = line):
                 
-                new_value = DictInfo.create(raw_key_data = line , ck2generator = generator)
+                new_value = DictInfo.create(raw_key_data = line,start_index=index , ck2generator = generator)
+                yield new_value
+                continue
+            
+            if OptionalKeyDict.corresponds(raw_info = line):
+                
+                new_value = OptionalKeyDict.create(raw_key_data = line,start_index=index , ck2generator = generator)
                 yield new_value
                 continue
     def get_path_generator(self) -> DataGeneratorFuncType:
